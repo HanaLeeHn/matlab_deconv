58247f586f91667b6b7ce0cbf1aff58b8987cda3
hanaleehn <hana.lee.hn@gmail.com>
2015-12-21 21:23:55 +0900

    deconvolution in frequency domain in case of a zero padded image

--- deconv_test.m
+++ deconv_test.m
@@ -1,22 +1,27 @@
 close all
+clear all
+clc
+
 im_org = imread('cameraman.tif');
 im_org = im2double(im_org);
 [ro,co] = size(im_org);
-im = zeros(3*ro, 3*co);
-[r,c] = size(im);
+
+r = 3*ro; 
+c = 3*co;
+im = zeros(r,c);
 im(ro+1:2*ro, co+1:2*co) = im_org;
 
 h = fspecial('gaussian', [11, 11], 2);
 h = h / sum(sum(h));
-H = fft2(h,r,c);
 
-g = imfilter(im, h, 'circular'); 
+% g = imfilter(im, h, 'circular'); 
+g = imfilter(im, h, 0); 
 G = fft2(g,r,c);
 
-H_mag = sqrt(real(H).^2+imag(H).^2);
-%H_mag = abs(H);
-restoredf = G.*conj(H)./(H_mag.^2);
-%restoredf = G./H;
+H = fft2(h,r,c);
+mag_H = sqrt(real(H).^2+imag(H).^2); % abs(H);
+Ghat = G.*conj(H)./(mag_H.^2); % G./H;
 
-restored = real(ifft2( restoredf ));
-figure, imshow(restored, [])
+ghat = real(ifft2( Ghat ));
+ghat_crop = ghat(252:507,252:507); % 252:507
+figure, imshow(ghat_crop, [])
